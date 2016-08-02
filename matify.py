4a4bd87678d8685bd447f75589f5ca73611b694f
Vishal Kuo <vishalkuo@hotmail.com>
2016-08-02 15:49:16 -0400

    Refined and error checkedS

--- matify.py
+++ matify.py
@@ -2,19 +2,29 @@ import sys
 import csv 
 import json
 
+
 def main():
 	data = None
 	if len(sys.argv) < 2:
-		print("Need an argument")
+		print("Need an argument for the file")
 		sys.exit(1)
-	with open('datasets/' + sys.argv[1]) as f:
-		data = f.read()
+	
+	try:
+		with open(sys.argv[1]) as f:
+			data = f.read()
+	except EnvironmentError:
+		print("File not found")
+		sys.exit(1)
 	data = data.split("\n")
 	#Trailing newline and headers are no fun
 	data = data[1:len(data) - 1]
-	data = map(lambda x : x.split(';'), data)
+	delimiter = ','
+	if len(sys.argv) >= 3:
+		delimiter = sys.argv[2]
+	data = map(lambda x : x.split(delimiter), data)
 	num_cols = len(data[0])
 	affected_set = []
+
 	for i in range(num_cols):
 		if (data[0][i].isdigit()):
 			continue	
@@ -32,8 +42,10 @@ def main():
 	with open("output.csv", "wb") as f:
     		writer = csv.writer(f)
 		writer.writerows(data)
+	print("Output written to: output.csv")
 	with open("legend.txt", "wb") as f:
 		json.dump(affected_set, f, indent=4)
+	print("Legend written to: legend.txt")
 
 if __name__ == '__main__':
 	main()
